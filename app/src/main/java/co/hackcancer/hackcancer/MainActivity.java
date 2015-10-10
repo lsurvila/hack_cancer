2ca0e9074fac10ea4953ae454924d65463d27b1a
Liudas Survila <liudas.survila@gmail.com>
2015-10-10 17:57:26 +0100

    - action bar colors

--- app/src/main/java/co/hackcancer/hackcancer/MainActivity.java
+++ app/src/main/java/co/hackcancer/hackcancer/MainActivity.java
@@ -39,7 +39,7 @@ public class MainActivity extends AppCompatActivity implements
     @Override
     protected void onCreate(Bundle savedInstanceState) {
         super.onCreate(savedInstanceState);
-        setContentView(R.layout.activity_main);
+        setContentView(R.layout.activity_main1);
 
         Toolbar toolbar = (Toolbar) findViewById(R.id.toolbar);
         setSupportActionBar(toolbar);
